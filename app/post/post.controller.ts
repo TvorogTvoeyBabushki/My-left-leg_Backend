a7f5dd10cc8e89869addda4ef529fbea8e77a6c0
Grimes <snake3773@mail.ru>
2023-07-30 20:32:45 +0300

    fix: поменял значение у post content при update

--- app/post/post.controller.ts
+++ app/post/post.controller.ts
@@ -84,7 +84,7 @@ export const updatePost = AsyncHandler(async (req: Request, res: Response) => {
 				description,
 				img,
 				categorysIds,
-				postContent: [postContent]
+				postContent
 			}
 		})
 
